8a3652e2ba8c5f8b084fbf43254f50c7f2ad00bf
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-07 04:04:23 -0500

    Convert to object

--- src/chess-notation.test.js
+++ src/chess-notation.test.js
@@ -1,12 +1,23 @@
 const {fromChessNotation} = require('./chess-notation')
 
-test('Too few characters returns an error', () => {
+test('Too few characters throws an error', () => {
     expect(() => fromChessNotation('N')).toThrow('Incorrect string length (N)');
 });
 
-test('Too many characters returns an error', () => {
+test('Too many characters throws an error', () => {
     expect(() => fromChessNotation('Ng8y')).toThrow('Incorrect string length (Ng8y)');
 })
+
+test('Valid move results in correct response', () => {
+    const obj = fromChessNotation('Ng8');
+    expect(obj.pieceName).toBe('Knight');
+    expect(obj.piece).toBe('N');
+    expect(obj.file).toBe('g');
+    expect(obj.x).toBe(6);
+    expect(obj.rank).toBe(8);
+    expect(obj.y).toBe(7);
+})
+
 
 // try {
 //     console.log(fromChessNotation('Ng8'));
